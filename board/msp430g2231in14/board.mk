428756f337d16c5b5d5d783da2d450ed6ff37d41
Rick Kimball <rick@kimballsoftware.com>
2016-06-22 10:08:54 -0400

    allow MCLK_FREQ override for g2231

--- board/msp430g2231in14/board.mk
+++ board/msp430g2231in14/board.mk
@@ -2,11 +2,12 @@
 # board/chip specific options
 #
 
+MCLK_FREQ ?= 16000000UL
 TOOLCHAIN = msp430-
 CORE = msp430
 CORE_FLAGS = -mmcu=msp430g2231
-C_DEFS = -DFABOOH -DF_CPU=16000000
-CXX_DEFS = -DFABOOH -DF_CPU=16000000
+C_DEFS = -DFABOOH -DF_CPU=$(MCLK_FREQ)
+CXX_DEFS = -DFABOOH -DF_CPU=$(MCLK_FREQ)
 LD_SCRIPT = $(FBD)$(BOARDDIR)/dummy.ld
 LD_FLAGS = $(CORE_FLAGS) -mdisable-watchdog -Wl,--gc-sections,-Map=$(OUT_DIR_F)$(PROJECT).map,-umain
 OPTIMIZATION += -ffunction-sections -fdata-sections
