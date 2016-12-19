891feb2a588e37d84f882b64538253b78b70444b
Rick Kimball <rick@kimballsoftware.com>
2016-12-18 23:59:19 -0500

    use new scheme of LED_BUILTIN pins.h define

--- examples/basic/blink/blink.cpp
+++ examples/basic/blink/blink.cpp
@@ -1,19 +1,26 @@
 /*
- * blink.cpp - fabooh blink
+ * blink.cpp - simple fabooh blink
  */
+
 #include <fabooh.h>
 #include <main.h>
 
+LED_BUILTIN_ LED_BUILTIN;
+
 void setup(void) {
-  RED_LED::setmode_output();
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // off
 }
 
 void loop(void) {
   const uint32_t freq = 2;
-  const uint32_t msec_delay = 1000 / freq / 2;
-
+  const uint32_t msec_delay0 = 1000 / freq / 10 ;
+  const uint32_t msec_delay1 = msec_delay0*9;
+  
   while (1) {
-    RED_LED::toggle();
-    delay(msec_delay);
+    digitalWrite(LED_BUILTIN, LOW);  // on
+    delay(msec_delay0);
+    digitalWrite(LED_BUILTIN, HIGH); // off
+    delay(msec_delay1);
   }
 }
