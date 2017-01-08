b9bcb1f7994994213d18d97f4c04582b9d6a04b2
Rick Kimball <rick@kimballsoftware.com>
2017-01-08 16:10:32 -0500

    Change default serial to USART2 for bluepill stm32f103 board

--- board/bluepill/pins.h
+++ board/bluepill/pins.h
@@ -6,9 +6,6 @@ typedef GPIO_PIN_LED<3,13,false> LED_BUILTIN_;
 typedef GPIO_PIN<1,9> TX1_PIN;
 typedef GPIO_PIN<1,10> RX1_PIN;
 
-typedef TX1_PIN TX_PIN;
-typedef RX1_PIN RX_PIN;
-
 typedef GPIO_PIN<1,2> TX2_PIN;
 typedef GPIO_PIN<1,3> RX2_PIN;
 
@@ -16,5 +13,8 @@ typedef GPIO_PIN<2,10> TX3_PIN;
 typedef GPIO_PIN<2,11> RX3_PIN;
 
 typedef GPIO_PIN<0,0xFFFF> NO_PIN;
+
+typedef TX2_PIN TX_PIN;
+typedef RX2_PIN RX_PIN;
 
 #endif
