56ff59551fce5003a964521beb0032cd6e46e7ab
Rick Kimball <rick@kimballsoftware.com>
2017-08-30 19:40:17 -0400

    adapt to new LED_BUILTIN pin naming

--- examples/basic/blink/blink.cpp
+++ examples/basic/blink/blink.cpp
@@ -3,24 +3,25 @@
  */
 
 #include <fabooh.h>
+#define SMALL_INT4
 #include <main.h>
 
-// instance the builtin LED
-LED_BUILTIN_ LED_BUILTIN; // GPIO_PIN_LED has on()/off() in addtion to low()/high()
+// instance the built-in LED
+LED_BUILTIN LED;
 
 void setup(void) {
-  LED_BUILTIN.setmode_output();
-  LED_BUILTIN.off();
+  LED.setmode_output();
+  LED.off();
 }
 
 void loop(void) {
-  const uint32_t Hz = 2;
-  const uint32_t msec_delay0 = 1000 / Hz / 2 ;
-  
+  static const uint32_t Hz = 1;
+  static const uint32_t msec_delay0 = 1000 / Hz / 2;
+
   while (1) {
-    LED_BUILTIN.on();
+    LED.on();
     delay(msec_delay0);
-    LED_BUILTIN.off();
+    LED.off();
     delay(msec_delay0);
   }
 }
