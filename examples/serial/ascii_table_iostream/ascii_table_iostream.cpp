4b05589d62239c750406b4b900084b315b0cfcd5
Rick Kimball <rick@kimballsoftware.com>
2017-08-30 19:44:43 -0400

    use print((char)) style on example

--- examples/serial/ascii_table_iostream/ascii_table_iostream.cpp
+++ examples/serial/ascii_table_iostream/ascii_table_iostream.cpp
@@ -15,7 +15,8 @@
 
 //------- File Globals ------
 namespace {
-  const uint32_t BAUD_RATE=9600;
+  static const uint32_t BAUD_RATE=9600;
+
   typedef serial_default_t<BAUD_RATE, CPU::frequency, TX_PIN, NO_PIN> serial;
   serial Serial;
   unsigned thisByte; // first visible ASCIIcharacter ' ' is number 32:
@@ -33,34 +34,22 @@ inline void setup(void)
 
 void loop()
 {
-
-  // either of these methods should end up with the same code size
-#if 1
   // use insertion operator style
-
-  Serial  << _RAW(thisByte)
-          << ", dec: " << thisByte
-          << ", oct: 0" << _OCT(thisByte)
+  Serial  << (char)(thisByte)
+          << ", dec: "   << thisByte
+          << ", oct: 0"  << _OCT(thisByte)
           << ", hex: 0x" << _HEX(thisByte)
           << ", bin: 0b" << _BIN(thisByte)
           << endl;
-#else
-  // use method call style
-
-  Serial.print(thisByte, RAW);
-  Serial.print(", dec: "); Serial.print(thisByte);
-  Serial.print(", oct: 0"); Serial.print(thisByte,OCT);
-  Serial.print(", hex: 0x"); Serial.print(thisByte,HEX);
-  Serial.print(", bin: 0b"); Serial.print(thisByte,BIN);
-  Serial.println();
-#endif
 
   // if printed last visible character '~' or 126, stop:
   if( thisByte == '~') {
     // This loop loops forever and does nothing
+
     while(1);
   }
 
   // go on to the next character
   thisByte++;
 }
+
